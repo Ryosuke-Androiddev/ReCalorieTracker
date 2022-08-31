38fb783fcebe7b2e9218337b334ccff6004683ad
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-09-01 01:10:39 +0900

    [develop] implement UseCase and Update it

---
+++ tracker/tracker_domain/src/main/java/com/plcoding/tracker_domain/use_case/ordinal/CalculateMealNutrients.kt
@@ -0,0 +1,110 @@
+package com.plcoding.tracker_domain.use_case.ordinal
+
+import com.plcoding.core.domain.model.ActivityLevel
+import com.plcoding.core.domain.model.Gender
+import com.plcoding.core.domain.model.GoalType
+import com.plcoding.core.domain.model.UserInfo
+import com.plcoding.core.domain.preferences.Preferences
+import com.plcoding.tracker_domain.model.MealType
+import com.plcoding.tracker_domain.model.TrackedFood
+import kotlin.math.roundToInt
+
+class CalculateMealNutrients(
+    private val preferences: Preferences
+) {
+    // MealNutrientsは、APIコール後のデータを引数にとって格納していく簡易かな
+    // これって引数はどっからとってくるんやろう → ローカルのデバイスに保存した奴から引っ張ってくるんや
+    // このロジック美しすぎる
+    operator fun invoke(trackedFoods: List<TrackedFood>): Result {
+        // keyごとに分類してる
+        val allNutrients = trackedFoods
+            .groupBy { it.mealType }
+            .mapValues { entry ->
+                val type = entry.key
+                val foods = entry.value
+                // どのくらい食べたかを保存した奴から全体でどのくらい食べたかの和をとってる
+                MealNutrients(
+                    carbs = foods.sumOf { it.carbs },
+                    protein = foods.sumOf { it.protein },
+                    fat = foods.sumOf { it.fat },
+                    calories = foods.sumOf { it.calories },
+                    mealType = type,
+                )
+            }
+        // Keyごとの合計値をとってる
+        val totalCarbs = allNutrients.values.sumOf { it.carbs }
+        val totalProtein = allNutrients.values.sumOf { it.protein }
+        val totalFat = allNutrients.values.sumOf { it.fat }
+        val totalCalories = allNutrients.values.sumOf { it.calories }
+
+        val userInfo = preferences.loadUserInfo()
+
+        // 1日のカロリー摂取量
+        val caloryGoal = dailyCalroryRequirement(userInfo)
+        // それぞれ消費カロリーで割って接種すべきカロリーを表示してる
+        val carbsGoal = (caloryGoal * userInfo.carbRatio / 4f).roundToInt()
+        val proteinGoal = (caloryGoal * userInfo.proteinRatio / 4f).roundToInt()
+        val fatGoal = (caloryGoal * userInfo.fatRatio / 9f).roundToInt()
+
+        return Result(
+            carbsGoal = carbsGoal,
+            proteinGoal = proteinGoal,
+            fatGoal = fatGoal,
+            caloriesGoal = caloryGoal,
+            totalCarbs = totalCarbs,
+            totalProtein = totalProtein,
+            totalFat = totalFat,
+            totalCalories = totalCalories,
+            mealNutrients = allNutrients
+        )
+    }
+
+    // 1日接種すべきカロリーをここで定義してる
+    private fun bmr(userInfo: UserInfo): Int {
+        return when (userInfo.gender) {
+            is Gender.Male -> {
+                (66.47f + 13.75f * userInfo.weight +
+                        5f * userInfo.height - 6.75f * userInfo.age).roundToInt()
+            }
+            is Gender.Female ->  {
+                (665.09f + 9.56f * userInfo.weight +
+                        1.84f * userInfo.height - 4.67 * userInfo.age).roundToInt()
+            }
+        }
+    }
+
+    // アクティブレベル、ゴールに応じてカロリー量を変化
+    private fun dailyCalroryRequirement(userInfo: UserInfo): Int {
+        val activityFactor = when(userInfo.activityLevel) {
+            is ActivityLevel.Low -> 1.2f
+            is ActivityLevel.Medium -> 1.3f
+            is ActivityLevel.High -> 1.4f
+        }
+        val caloryExtra = when(userInfo.goalType) {
+            is GoalType.LoseWeight -> -500
+            is GoalType.KeepWeight -> 0
+            is GoalType.GainWeight -> 500
+        }
+        return (bmr(userInfo) * activityFactor + caloryExtra).roundToInt()
+    }
+
+    data class MealNutrients(
+        val carbs: Int,
+        val protein: Int,
+        val fat: Int,
+        val calories: Int,
+        val mealType: MealType
+    )
+
+    data class Result(
+        val carbsGoal: Int,
+        val proteinGoal: Int,
+        val fatGoal: Int,
+        val caloriesGoal: Int,
+        val totalCarbs: Int,
+        val totalProtein: Int,
+        val totalFat: Int,
+        val totalCalories: Int,
+        val mealNutrients: Map<MealType, MealNutrients>
+    )
+}
