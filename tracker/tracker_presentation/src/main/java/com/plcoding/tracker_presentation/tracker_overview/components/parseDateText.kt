9881aada225c6e18ba5e6d98ab434128e23146f8
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-09-04 00:52:43 +0900

    [develop] change date format

--- tracker/tracker_presentation/src/main/java/com/plcoding/tracker_presentation/tracker_overview/components/parseDateText.kt
+++ tracker/tracker_presentation/src/main/java/com/plcoding/tracker_presentation/tracker_overview/components/parseDateText.kt
@@ -13,6 +13,6 @@ fun parseDateText(date: LocalDate): String {
         today -> stringResource(id = R.string.today)
         today.minusDays(1) -> stringResource(id = R.string.yesterday)
         today.plusDays(1) -> stringResource(id = R.string.tomorrow)
-        else -> DateTimeFormatter.ofPattern("dd LLLL").format(date)
+        else -> DateTimeFormatter.ofPattern("M月 dd日").format(date)
     }
 }
