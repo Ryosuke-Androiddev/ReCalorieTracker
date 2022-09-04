c4eccab1c264b42ea66858ddb54f6008363f0d33
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2022-09-04 21:30:28 +0900

    [develop] Implement SearchViewModel

---
+++ tracker/tracker_presentation/src/main/java/com/plcoding/tracker_presentation/search/original/SearchState.kt
@@ -0,0 +1,10 @@
+package com.plcoding.tracker_presentation.search.original
+
+import com.plcoding.tracker_presentation.search.TrackableFoodUiState
+
+data class SearchState(
+    val query: String = "",
+    val isHintVisible: Boolean = false,
+    val isSearching: Boolean = false,
+    val trackableFood: List<TrackableFoodUiState> = emptyList()
+)
